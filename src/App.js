a8a5f74796d4dd14ddd6aeb38bdb0f23f9877bbb
abhioverhere <abhiabhilashpat@gmail.com>
2023-11-13 00:30:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Main from './ui-components/Main.jsx';
+import Dash from './Elements/Dash.jsx';
+import List from './Elements/List.jsx';
+import Form from './Elements/Form.jsx';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/EmpAPP" element={<Dash/>}/>
+        <Route path="/detail" element={<Main child ={<List/>}/>}/>
+        <Route path="/add" element={<Main child ={<Form/>}/>}/>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
